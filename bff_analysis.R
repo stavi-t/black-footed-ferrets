db3b49cc7626b54f637a161c9224e628446eb664
Stavi Tennenbaum <stavrt@vpn12-client-172-20-211-203.princeton.edu>
2022-03-21 14:20:49 -0400

    wrote merge_scaffolds function

--- bff_analysis.R
+++ bff_analysis.R
@@ -1,4 +1,4 @@
-setwd("/Users/stavrt/ferret")
+setwd("/Users/stavrt/ferret/")
 library(VennDiagram)
 library(wesanderson)
 
@@ -23,7 +23,6 @@ write.table(dfals_S_sig,file="bff_dfals_sperm5_sig.txt",sep="\t",dec=".",row.nam
 write.table(dfals_T_sig,file="bff_dfals_testes9_sig.txt",sep="\t",dec=".",row.names=FALSE,col.names=TRUE)
 write.table(dfT_sig,file="bff_dffirm_testes9_sig.txt",sep="\t",dec=".",row.names=FALSE,col.names=TRUE)
 write.table(dfS_sig,file="bff_dfcount_sperm5_sig.txt",sep="\t",dec=".",row.names=FALSE,col.names=TRUE)
-
 
 # VENN DIAGRAMS: overlap of significant DMS from 5 MACAU analyses
 vector_als_blood <- as.vector(dfals_B_sig$id)
@@ -83,25 +82,45 @@ venn.diagram(x=list(vector_als_testes,vector_firm_testes ),
              cat.fontface = "bold",
              cat.default.pos="outer")
 
-# changing scaffold names to scaffold id numbers for site annt
-# about 100 DMS that did not get assigned scaffold #s from merge
-macau_out = read.table("bff_dfals_blood8_sig.TESTSITES", header=FALSE)
-dim(macau_out_5)
+# ***changing scaffold names to scaffold id numbers for site annt***
+# omits 'N/A' DMS that don't get assigned scaffold #s from merge 
+## (one way merge - omits many sites! make sure you want to do this!) ##
+setwd("/Users/stavrt/ferret/site_annt/MACAU-raw/")
+test_sites = read.table("MACAU_output_avglittersize_blood8.assoc.clean.TESTSITES", 
+                        header=FALSE)
+dim(test_sites)
 dim(final_macau_out)
 
-scaffold_key = read.table("fake_chrs_musputfur1.key", header=FALSE)
-head(macau_out_5$V1)
+scaffold_key = read.table("/Users/stavrt/ferret/fake_chrs_musputfur1.key", header=FALSE)
+head(test_sites$V1)
 head(scaffold_key$V1)
 
-# data file x, key file y
-merged_macau_out = merge(macau_out, scaffold_key, by.x="V1", by.y="V1", all.x=TRUE)
-head(merged_macau_out)
-merged_macau_out[merged_macau_out$V1=="AEYP01108960",]
-final_macau_out = merged_macau_out[,c("V2.y", "V2.x")]
+# data file x (V1, V2), key file y (V1, V2)
+merge_scaffolds <- function(test_sites, scaffold_key) {
+  macau_file = read.table(test_sites, header=FALSE)
+  names(scaffold_key)
+  merged_macau_out = merge(macau_file, scaffold_key, by.x="V1", by.y="V1", all.x=FALSE)
+  final_macau_out = merged_macau_out[,c("V2.y", "V2.x")]
+  
+  out_file = paste0("query_", test_sites)
+  out_file = gsub(".TESTSITES", ".txt", out_file)
+  write.table(final_macau_out,file=paste0("/Users/stavrt/ferret/site_annt/MACAU-raw/", out_file),
+                                          sep="\t",dec=".",row.names=FALSE,col.names=FALSE)
+}
 
-head(final_macau_out)
-write.table(final_macau_out,file="query_MACAU_dfcount_sperm5_sig_fake_chr.txt",
-            sep="\t",dec=".",row.names=FALSE,col.names=FALSE)
+test_site_list <-
+  list.files(pattern=".TESTSITES")
+lapply(test_site_list, merge_scaffolds, scaffold_key=scaffold_key)
 
+merge_scaffolds(test_site_list, scaffold_key)
 
+# merge_scaffolds("MACAU_output_avglittersize_blood8.assoc.clean.TESTSITES", scaffold_key)
+# merge_scaffolds("MACAU_output_avglittersize_sperm5.assoc.clean.TESTSITES", scaffold_key)
+# merge_scaffolds("MACAU_output_avglittersize_testes9.assoc.clean.TESTSITES", scaffold_key)
+# merge_scaffolds("MACAU_output_firmness_testes9.assoc.clean.TESTSITES", scaffold_key)
+# merge_scaffolds("MACAU_output_spermcount_sperm5.assoc.clean.TESTSITES", scaffold_key)
+
+# checking for replicate values
+#merged_macau_out[merged_macau_out$V1=="AEYP01108960",]
+  
 
